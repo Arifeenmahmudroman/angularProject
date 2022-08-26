4d6de978088dc1116945e5f37ea4c14fad9cc8f4
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2022-08-26 20:25:40 +0600

    chart login dashboard

---
+++ src/app/components/team/team.component.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team',
+  templateUrl: './team.component.html',
+  styleUrls: ['./team.component.css']
+})
+export class TeamComponent implements OnInit {
+
+  
+  users: any;
+
+  use = ['a', 'b', 'c'];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    let resp = this.http.get('https://jsonplaceholder.typicode.com/users');
+    resp.subscribe((data) => this.users = data);
+  }
+}
